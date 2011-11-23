aceed97f794c64155ca617b98cb2657ff6084194
George Kurian <gkurian@mit.edu>
2011-11-22 20:15:33 -0500

    [tests] Added more shared memory unit tests

--- tests/unit/shared_mem_test1/shared_mem_test1.cc
+++ tests/unit/shared_mem_test1/shared_mem_test1.cc
@@ -1,63 +1,134 @@
 #include "core.h"
 #include "mem_component.h"
 #include "core_manager.h"
+#include "event_manager.h"
 #include "simulator.h"
-
+#include "event.h"
 #include "carbon_user.h"
 #include "fixed_types.h"
 
 using namespace std;
 
+IntPtr address = 0x1000;
+
+UInt32 write_val_0;
+UInt32 read_val_0;
+UInt32 write_val_1;
+UInt32 read_val_1;
+
+typedef struct
+{
+   core_id_t core_id;
+   Core::mem_op_t mem_op_type;
+   IntPtr address;
+   Byte* buf;
+   UInt32 size;
+   UInt32 value;
+} MemoryRequest;
+
+void initiateMemoryAccess(UInt64 time, UInt32 access_id);
+void completeMemoryAccess(Event* event);
+void waitForCompletion();
+
+MemoryRequest memory_requests[] = 
+{
+   { 0, Core::WRITE, address, (Byte*) &write_val_0, sizeof(write_val_0), 100 },
+   { 0, Core::READ,  address, (Byte*) &read_val_0,  sizeof(read_val_0),  100 },
+   { 1, Core::READ,  address, (Byte*) &read_val_1,  sizeof(read_val_1),  100 },
+   { 1, Core::WRITE, address, (Byte*) &write_val_1, sizeof(write_val_1), 110 },
+   { 0, Core::READ,  address, (Byte*) &read_val_0,  sizeof(read_val_0),  110 }
+};
+
+
+void initiateMemoryAccess(UInt64 time, UInt32 access_id)
+{
+   if (access_id == 5)
+      return;
+
+   Core* core = Sim()->getCoreManager()->getCoreFromID(memory_requests[access_id].core_id);
+   Core::mem_op_t mem_op_type = memory_requests[access_id].mem_op_type;
+   IntPtr address = memory_requests[access_id].address;
+   Byte* buf = memory_requests[access_id].buf;
+   UInt32 size = memory_requests[access_id].size;
+   UInt32 value = memory_requests[access_id].value;
+   if (mem_op_type == Core::WRITE)
+      *((UInt32*) buf) = value;
+
+   UnstructuredBuffer* event_args = new UnstructuredBuffer();
+   (*event_args) << core
+                 << access_id
+                 << MemComponent::L1_DCACHE << Core::NONE << mem_op_type
+                 << address << buf << size
+                 << true;
+   EventInitiateMemoryAccess* event = new EventInitiateMemoryAccess(time, event_args);
+   Event::processInOrder(event, core->getId(), EventQueue::ORDERED);
+}
+
+void completeMemoryAccess(Event* event)
+{
+   Core* core;
+   UInt32 access_id;
+   UInt64 time = event->getTime();
+   UnstructuredBuffer* event_args = event->getArgs();
+   (*event_args) >> core >> access_id;
+   assert(core);
+
+   Core::mem_op_t mem_op_type = memory_requests[access_id].mem_op_type;
+   Byte* buf = memory_requests[access_id].buf;
+   UInt32 value = memory_requests[access_id].value;
+
+   if (mem_op_type == Core::READ)
+   {
+      LOG_ASSERT_ERROR(*((UInt32*) buf) == value, "Expected(%u) != Actual(%u)",
+                       value, *((UInt32*) buf));
+   }
+
+   access_id ++;
+   initiateMemoryAccess(time, access_id);
+}
+
+void waitForCompletion()
+{
+   // Sleep in a loop for 100 milli-sec each. Wake up and see if done
+   while (1)
+   {
+      usleep(1000);
+      if (! Sim()->getEventManager()->hasEventsPending())
+         break;
+   }
+}
+
 int main (int argc, char *argv[])
 {
    printf("Starting (shared_mem_test1)\n");
+
+   // Start the simulator
    CarbonStartSim(argc, argv);
 
-   UInt32 address = 0x1000;
-
    // Enable Performance Models
-   for (UInt32 i = 0; i < Config::getSingleton()->getTotalCores(); i++)
-      Sim()->getCoreManager()->getCoreFromID(i)->enablePerformanceModels();
+   Simulator::__enablePerformanceModels();
 
-   // 1) Get a core object
-   Core* core_0 = Sim()->getCoreManager()->getCoreFromID(0);
-   Core* core_1 = Sim()->getCoreManager()->getCoreFromID(1);
+   // Register Event Handlers
+   Event::registerHandler(Event::COMPLETE_MEMORY_ACCESS, completeMemoryAccess);
 
-   UInt32 write_val_0 = 100;
-   UInt32 read_val_0 = 0;
+   initiateMemoryAccess(0, 0);
 
-   // Core 0 - Write value into this address
-   printf("Writing(%u) into address(0x%x)\n", write_val_0, address);
-   core_0->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::WRITE, address, (Byte*) &write_val_0, sizeof(write_val_0));
+   // Wait till no more events are present
+   waitForCompletion();
 
-   // Core 0 - Read out the value
-   core_0->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::READ, address, (Byte*) &read_val_0, sizeof(read_val_0));
-   printf("Read Out(%u) from address(0x%x)\n", read_val_0, address);
-   assert(read_val_0 == 100);
-
-   UInt32 write_val_1 = 0;
-   UInt32 read_val_1 = 0;
-
-   // Core 1 - Read out the value and write something else
-   core_1->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::READ, address, (Byte*) &read_val_1, sizeof(read_val_1));
-   printf("Read Out(%u) from address(0x%x)\n", read_val_1, address);
-   assert(read_val_1 == 100);
-
-   write_val_1 = read_val_1 + 10;
-   // Core 1 - Write read out value + 1
-   printf("Writing(%u) into address(0x%x)\n", write_val_1, address);
-   core_1->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::WRITE, address, (Byte*) &write_val_1, sizeof(write_val_1));
-   
-   // Core 0 - Read out the value
-   core_0->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::READ, address, (Byte*) &read_val_0, sizeof(read_val_0));
-   printf("Read Out(%u) from address(0x%x)\n", read_val_0, address);
-   assert(read_val_0 == 110);
+   // Unregister Event Handlers
+   Event::unregisterHandler(Event::COMPLETE_MEMORY_ACCESS);
 
    // Disable Performance Models
-   for (UInt32 i = 0; i < Config::getSingleton()->getTotalCores(); i++)
-      Sim()->getCoreManager()->getCoreFromID(i)->disablePerformanceModels();
+   Simulator::__disablePerformanceModels();
+   
+   // Stop the simulator
+   CarbonStopSim();
    
    printf("Finished (shared_mem_test1) - SUCCESS\n");
-   CarbonStopSim();
    return 0;
+}
+   
+void accessMemory(UInt64 time, Core* core, Core::mem_op_t mem_op_type, IntPtr address, Byte* buf, UInt32 size)
+{
 }
--- tests/unit/shared_mem_test2/shared_mem_test2.cc
+++ tests/unit/shared_mem_test2/shared_mem_test2.cc
@@ -2,67 +2,225 @@
 #include "mem_component.h"
 #include "core_manager.h"
 #include "simulator.h"
-
-#include "carbon_user.h"
+#include "event.h"
+#include "event_manager.h"
+#include "utils.h"
 #include "fixed_types.h"
+#include "carbon_user.h"
 
 using namespace std;
 
-void* thread_func(void*);
+void initializeMemory();
+void startSimulation(UInt64 time);
+void checkResult();
+void __checkResult();
+void initiateMemoryAccess(Core* core, UInt64 time, UInt32 access_id);
+void completeMemoryAccess(Event* event);
+void waitForCompletion();
+void printHelpMessage();
 
-IntPtr address = 0x1000;
-int num_threads = 100;
-int num_iterations = 100;
+IntPtr _address = 0x1000;
+UInt32 _num_cores = 100;
+UInt32 _num_iterations = 100;
+vector<UInt32> _buf_list;
+UInt64 _max_time = 0;
 
 int main (int argc, char *argv[])
 {
-   CarbonStartSim(argc, argv);
    printf("Starting (shared_mem_test2)\n");
 
-   carbon_thread_t tid_list[num_threads];
+   // Start the simulator
+   CarbonStartSim(argc, argv);
 
-   Core* core = Sim()->getCoreManager()->getCurrentCore();
-   int val = 0;
-   core->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::WRITE, address, (Byte*) &val, sizeof(val));
-
-   for (int i = 0; i < num_threads; i++)
+   // Enable the performance models
+   Simulator::__enablePerformanceModels();
+ 
+   // Read Command Line Arguments
+   for (SInt32 i = 1; i < argc-1; i += 2)
    {
-      tid_list[i] = CarbonSpawnThread(thread_func, (void*) i);
+      if (string(argv[i]) == "-i")
+         _num_iterations = atoi(argv[i+1]);
+      else if (string(argv[i]) == "-c") // Simulator arguments
+         break;
+      else if (string(argv[i]) == "-h")
+      {
+         printHelpMessage();
+         exit(0);
+      }
+      else
+      {
+         fprintf(stderr, "** ERROR **\n");
+         printHelpMessage();
+         exit(-1);
+      }
    }
 
-   for (int i = 0; i < num_threads; i++)
+   // Get the number of cores from the config object
+   _num_cores = Config::getSingleton()->getTotalCores();
+
+   // Register Event Handlers
+   Event::registerHandler(Event::COMPLETE_MEMORY_ACCESS, completeMemoryAccess);
+
+   _buf_list.resize(_num_cores, 0);
+
+   // Initialize memory and start simulation after memory is initialized
+   initializeMemory();
+   waitForCompletion();
+   
+   // Check the final result
+   checkResult();
+   waitForCompletion();
+  
+   // Unregister Event Handlers
+   Event::unregisterHandler(Event::COMPLETE_MEMORY_ACCESS);
+
+   // Disable Performance Models
+   Simulator::__disablePerformanceModels();
+
+   // Stop the simulator 
+   CarbonStopSim();
+
+   return 0;
+}
+
+void printHelpMessage()
+{
+   fprintf(stderr, "[Usage]: ./shared_mem_test2 -i <number of iterations>\n");
+}
+
+void initializeMemory()
+{
+   _buf_list[0] = 0;
+   Byte* buf = (Byte*) &_buf_list[0];
+   UInt32 size = sizeof(_buf_list[0]);
+   Core* core = Sim()->getCoreManager()->getCoreFromID(0);
+   assert(core);
+
+   UnstructuredBuffer* event_args = new UnstructuredBuffer();
+   (*event_args) << core
+                 << 0
+                 << MemComponent::L1_DCACHE << Core::NONE << Core::WRITE
+                 << _address << buf << size
+                 << true;
+   EventInitiateMemoryAccess* event = new EventInitiateMemoryAccess(0, event_args);
+   Event::processInOrder(event, 0, EventQueue::ORDERED);
+}
+
+void startSimulation(UInt64 time)
+{
+   LOG_PRINT("startSimulation(%llu)", time);
+   for (UInt32 i = 1; i < _num_cores; i++)
    {
-      CarbonJoinThread(tid_list[i]);
+      Core* core = Sim()->getCoreManager()->getCoreFromID(i);
+      assert(core);
+      initiateMemoryAccess(core, time, 0);
    }
-   
-   core->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::NONE, Core::READ, address, (Byte*) &val, sizeof(val));
-   
-   printf("val(%i)\n", val);
-   if (val == (num_threads * num_iterations))
+}
+
+void checkResult()
+{
+   Byte* buf = (Byte*) &_buf_list[0];
+   UInt32 size = sizeof(_buf_list[0]);
+   Core* core = Sim()->getCoreManager()->getCoreFromID(0);
+   assert(core);
+
+   UnstructuredBuffer* event_args = new UnstructuredBuffer();
+   (*event_args) << core
+                 << 1
+                 << MemComponent::L1_DCACHE << Core::NONE << Core::READ
+                 << _address << buf << size
+                 << true;
+   EventInitiateMemoryAccess* event = new EventInitiateMemoryAccess(_max_time, event_args);
+   Event::processInOrder(event, 0, EventQueue::ORDERED);
+}
+
+void __checkResult()
+{
+   printf("Final Answer(%u)\n", _buf_list[0]);
+   if (_buf_list[0] == ((_num_cores-1) * _num_iterations))
    {
       printf("shared_mem_test2 (SUCCESS)\n");
    }
    else
    {
       printf("shared_mem_test2 (FAILURE)\n");
-      assert(false);
+      LOG_PRINT_ERROR("Abort");
    }
-   
-   CarbonStopSim();
-   return 0;
 }
 
-void* thread_func(void*)
+void initiateMemoryAccess(Core* core, UInt64 time, UInt32 access_id)
 {
-   Core* core = Sim()->getCoreManager()->getCurrentCore();
+   LOG_PRINT("initiateMemoryAccess[Core Id(%i), Time(%llu), Access Id(%u)]", core->getId(), time, access_id);
+   if (access_id == (2 * _num_iterations))
+      return;
 
-   for (int i = 0; i < num_iterations; i++)
+   Core::mem_op_t mem_op_type;
+   Core::lock_signal_t lock_signal;
+   if (access_id % 2 == 0)
    {
-      int val;
-      core->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::LOCK, Core::READ_EX, address, (Byte*) &val, sizeof(val));
-      
-      val += 1;
+      mem_op_type = Core::READ_EX;
+      lock_signal = Core::LOCK;
+   }
+   else
+   {
+      mem_op_type = Core::WRITE;
+      lock_signal = Core::UNLOCK;
+   }
 
-      core->initiateMemoryAccess(0, MemComponent::L1_DCACHE, Core::UNLOCK, Core::WRITE, address, (Byte*) &val, sizeof(val));
+   Byte* buf = (Byte*) &_buf_list[core->getId()];
+   UInt32 size = sizeof(_buf_list[core->getId()]);
+
+   UnstructuredBuffer* event_args = new UnstructuredBuffer();
+   (*event_args) << core
+                 << access_id
+                 << MemComponent::L1_DCACHE << lock_signal << mem_op_type
+                 << _address << buf << size
+                 << true;
+   EventInitiateMemoryAccess* event = new EventInitiateMemoryAccess(time, event_args);
+   Event::processInOrder(event, core->getId(), EventQueue::ORDERED);
+}
+
+void completeMemoryAccess(Event* event)
+{
+   Core* core;
+   UInt32 access_id;
+   UInt64 time = event->getTime();
+   UnstructuredBuffer* event_args = event->getArgs();
+   (*event_args) >> core >> access_id;
+   assert(core);
+
+   LOG_PRINT("Complete Memory Access[Core(%i), Time(%llu), Access Id(%u)]", core->getId(), time, access_id);
+
+   _max_time = getMax<UInt64>(_max_time, time);
+
+   if (core->getId() == 0)
+   {
+      if (access_id == 0)
+         startSimulation(time);
+      else if (access_id == 1)
+         __checkResult();
+      else
+         LOG_PRINT_ERROR("Unrecognized Access Id(%u)", access_id);
+   }
+   else
+   {
+      if (access_id % 2 == 0)
+      {
+         // This is a READ_EX. Increment variable by 1
+         _buf_list[core->getId()] ++;
+      }
+      access_id ++;
+      initiateMemoryAccess(core, time, access_id);
+   }
+}
+
+void waitForCompletion()
+{
+   // Sleep in a loop for 1 milli-sec each. Wake up and see if done
+   while (1)
+   {
+      usleep(1000);
+      if (! Sim()->getEventManager()->hasEventsPending())
+         break;
    }
 }
